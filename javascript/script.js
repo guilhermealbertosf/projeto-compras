47d8458139a8d66d314dc46e30d519a748242736
Guilherme Alberto <gmightasf@gmail.com>
2025-03-08 10:45:46 -0300

    test remove script

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-let total = 0
-function somar(){
-    let val = document.getElementById("txtval")
-    let res = document.getElementById("res")
-    let n = Number(val.value)
-    total += n
-    res.innerHTML = `O valor total da compra é R$ ${total.toFixed(2)}`
-}

47fe1df6dbc809d36c376f37569952f1182f656d
Guilherme Alberto <gmightasf@gmail.com>
2025-04-23 20:40:21 -0300

    Simplicity

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,17 @@
 let total = 0
 let c = 0
+let valores = [0]
 function somar(){
     let produto = document.getElementById("itxtn").value
     let val = document.getElementById("txtval")
     let res = document.getElementById("res")
     let tab = document.getElementById("tab")
     let n = Number(val.value)
+
     total += n
+    valores.push(n)
     c += 1
+
     res.innerHTML = `O valor total da compra é R$ ${total.toFixed(2)}`
     let elemento = document.createElement('option')
     elemento.value = `${c}`
@@ -19,16 +23,26 @@ function somar(){
 
 
 
-// Falta sinconizar o valor da tabela com o do resultado!!
+// Falta sinconizar o valor da tabela com o do resultado!! Tentar depurar!
 
 
-
+/*
 function del(){
     let produto = document.getElementById("itxtn").value
-    let codigo = document.getElementById("idel").value
+    let txtcod = document.getElementById("idel")
     let val = document.getElementById("txtval")
+
+    let codigo = Number(txtcod.value)
     let n = Number(val.value)
+//problema
+    let desc = valores[codigo]
+
+    total -= desc
+    total += n
+//problema
     let elemento = document.getElementById(`opcao${codigo}`)
     elemento.text = `${codigo}º - ${produto} - R$ ${n.toFixed(2)}`
-    total += n
+    
+    res.innerHTML = `O valor total da compra é R$ ${total.toFixed(2)}`
 }
+*/
